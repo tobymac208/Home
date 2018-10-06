1e23838da917fba141b32049cf7ac2058563f1f4
Nik <ironman_fern@comcast.net>
2018-10-06 18:54:25 -0500

    Small additions.

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        String name = "Home";
+        String version = "v1.0";
+        String windowTitle = name + " " + version;
+
         Parent root = FXMLLoader.load(getClass().getResource("Windows/Login/main.fxml"));
         // set the title of this window
-        primaryStage.setTitle("Home v1.0");
+        primaryStage.setTitle(windowTitle);
         // load the file from our root object
         primaryStage.setScene(new Scene(root, 400, 400));
         // display the window
